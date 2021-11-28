b8a225eb6bf959a7442c7fadccdde46ef092385e
trevin-livele <liveletrevin6@gmail.com>
2021-11-28 12:35:43 +0300

    added search script

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ left.addEventListener('mouseleave', () => container.classList.remove('hover-left
 
 right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
 right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
+
+
+
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn-search')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
