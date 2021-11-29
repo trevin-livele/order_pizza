b2c2c870f1b533a8b09e1c934d810f9d0275596f
trevin-livele <liveletrevin6@gmail.com>
2021-11-29 23:27:37 +0300

    refactored js code and nav bar

--- js/script.js
+++ js/script.js
@@ -10,43 +10,4 @@ right.addEventListener('mouseleave', () => container.classList.remove('hover-rig
 
 
 
-const search = document.querySelector('.search')
-const btn = document.querySelector('.btn-search')
-const input = document.querySelector('.input')
 
-btn.addEventListener('click', () => {
-    search.classList.toggle('active')
-    input.focus()
-})
-
-
-
-const ratings = document.querySelectorAll('.rating')
-const ratingsContainer = document.querySelector('.ratings-container')
-const sendBtn = document.querySelector('#send')
-const panel = document.querySelector('#panel')
-let selectedRating = 'Satisfied'
-
-ratingsContainer.addEventListener('click', (e) => {
-    if(e.target.parentNode.classList.contains('rating')) {
-        removeActive()
-        e.target.parentNode.classList.add('active')
-        selectedRating = e.target.nextElementSibling.innerHTML
-    }
-})
-
-sendBtn.addEventListener('click', (e) => {
-    panel.innerHTML = `
-        <i class="fas fa-heart"></i>
-        <strong>Thank You!</strong>
-        <br>
-        <strong>Feedback: ${selectedRating}</strong>
-        <p>We'll use your feedback to improve our customer support</p>
-    `
-})
-
-function removeActive() {
-    for(let i = 0; i < ratings.length; i++) {
-        ratings[i].classList.remove('active')
-    }
-}
